dc2c32ce6dfc13c201463a3196e8b7f4fa7ed54d
Neo <neotheprogramist@proton.me>
2024-02-06 15:57:30 +0000

    prettify prints

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+print_in_blue() {
+    echo -e "\033[1;34m$1\033[0m"
+}
+
+print_in_blue "Building the project..." && \
 ./scripts/build.sh && \
+
+print_in_blue "Compiling the project..." && \
 ./scripts/compile.sh && \
+
+print_in_blue "Running the project..." && \
 ./scripts/run.sh && \
+
+print_in_blue "Proving the project..." && \
 ./scripts/stone-prove.sh && \
+
+print_in_blue "Verifying the project..." && \
 ./scripts/stone-verify.sh
