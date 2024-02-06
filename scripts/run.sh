0eff7f38af121b42f96512dcd274e8d0a83df5c3
Neo <neotheprogramist@proton.me>
2024-02-06 19:52:38 +0000

    recursive layout with keccak instead of blake2s

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ cd cairo-vm && \
 cargo build --release -p cairo-vm-cli && \
 cargo run --release -p cairo-vm-cli -- \
   ../resources/main_compiled.json \
-  --layout starknet_with_keccak \
+  --layout recursive_large_output \
   --print_output \
   --trace_file ../resources/main_trace.bin \
   --memory_file ../resources/main_memory.bin \
