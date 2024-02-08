a478d22165ceae6303aa2eaf09b0ed5c9b429a7a
Neo <neotheprogramist@proton.me>
2024-02-08 13:31:40 +0000

    fix script

--- scripts/cairo-verify.sh
+++ scripts/cairo-verify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd cairo-verifier-runner && \
-cargo run -- verify ../cairo-verifier/target/dev/cairo_verifier.sierra.json < ../resources/main_proof.json && \
+cargo run -- ../cairo-verifier/target/dev/cairo_verifier.sierra.json < ../resources/main_proof.json && \
 cd ..
