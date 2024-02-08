89bed9fcf2c9b5d422a773094340bb34e9a4b5ca
Neo <neotheprogramist@proton.me>
2024-02-08 11:53:38 +0000

    use python cairo-run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-cd cairo-vm && \
-cargo build --release -p cairo-vm-cli && \
-cargo run --release -p cairo-vm-cli -- \
-  ../resources/main_compiled.json \
-  --layout recursive_large_output \
+# cd cairo-vm && \
+# cargo build --release -p cairo-vm-cli && \
+# cargo run --release -p cairo-vm-cli -- \
+#   ../resources/main_compiled.json \
+#   --layout recursive_large_output \
+#   --print_output \
+#   --trace_file ../resources/main_trace.bin \
+#   --memory_file ../resources/main_memory.bin \
+#   --air_public_input ../resources/main_public_input.json \
+#   --air_private_input ../resources/main_private_input.json \
+#   --proof_mode && \
+# cd ..
+
+source .venv/bin/activate && \
+pip install --upgrade pip && \
+pip install -r requirements.txt && \
+cairo-run \
+  --program resources/main_compiled.json \
+  --layout recursive \
   --print_output \
-  --trace_file ../resources/main_trace.bin \
-  --memory_file ../resources/main_memory.bin \
-  --air_public_input ../resources/main_public_input.json \
-  --air_private_input ../resources/main_private_input.json \
+  --trace_file resources/main_trace.bin \
+  --memory_file resources/main_memory.bin \
+  --air_public_input resources/main_public_input.json \
+  --air_private_input resources/main_private_input.json \
   --proof_mode && \
-cd ..
+deactivate
