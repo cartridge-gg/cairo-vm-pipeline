546dbd7df5983f6e6e1d7e51ec992e9799d4bbdb
Neo <neotheprogramist@proton.me>
2024-02-08 11:40:25 +0000

    new parser pipeline

--- scripts/cairo-verify.sh
+++ scripts/cairo-verify.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd cairo-verifier && \
-scarb build && \
-cargo run --release -- ./target/dev/cairo_verifier.sierra.json < ../resources/main_proof_parsed.txt
+cd cairo-verifier-runner && \
+cargo run -- verify ../cairo-verifier/target/dev/cairo_verifier.sierra.json < ../resources/main_proof.json && \
 cd ..
