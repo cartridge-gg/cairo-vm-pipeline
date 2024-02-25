d9990d59bcb12c93473093a503ec97c496cdb9f0
Neo <neotheprogramist@proton.me>
2024-02-25 21:50:30 +0000

    use new parser

--- scripts/parse.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd cairo-lang && \
-source .venv/bin/activate && \
-pip install --upgrade pip && \
-pip install -r requirements.txt && \
-python src/main.py -l starknet < ../resources/main_proof.json > ../resources/main_proof_parsed.txt && \
-deactivate && \
-cd ..
