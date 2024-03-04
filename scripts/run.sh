53de9576d8daf15ddc4a49fce509a58bd3da385a
Neo <neotheprogramist@proton.me>
2024-03-05 00:51:40 +0100

    fix script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 source .venv/bin/activate && \
-cd resources && \
 cairo-run \
     --program resources/main_compiled.json \
-    --layout resources/recursive \
+    --layout recursive \
     --program_input resources/main_input.json \
     --air_public_input resources/main_public_input.json \
     --air_private_input resources/main_private_input.json \
@@ -12,5 +11,4 @@ cairo-run \
     --memory_file resources/main_memory.bin \
     --print_output \
     --proof_mode && \
-cd .. && \
 deactivate
